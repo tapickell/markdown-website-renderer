b2838d586527c4f89c7283eb9ba17950ea3b0558
Todd Pickell <myappleguy@gmail.com>
2013-10-09 11:46:04 -0500

    got live search to work by parsing json hash to array of values

--- public/js/web.js
+++ public/js/web.js
@@ -1,6 +1,30 @@
-var support_app = angular.module('portal', []).
-	config([]);
+var support_app = angular.module('portal', ['$strap.directives']).
+	config(['$routeProvider', '$locationProvider', function app_route_config($routeProvider, $locationProvider) {
+		$routeProvider.
+			when('/', {
+				controller: SupportController,
+				templateUrl: 'articles.html'
+			})
+		$locationProvider.html5Mode(true);
+	}]);
 
-support_app.controller('support_controller', ['$scope', function ($scope) {
-	$scope.content = 
-}]);
+function SupportController($scope) {
+	$scope.get_articles = function() {
+		$.ajax({
+			dataType: 'json',
+			url: '/content.json',
+			method: 'GET',
+			success: function(reply) {
+				$scope.$apply(function() {
+					var values = [];
+					for (var key in reply) {
+						values.push(reply[key]);
+					};
+					$scope.articles = values;
+				});
+			}
+		});
+	};
+
+	$scope.get_articles();
+}
